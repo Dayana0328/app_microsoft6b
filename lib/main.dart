131fb8a5cb4023dcc2d50f22b5f3e600c06e1275
DannyJativa <dany.jativa.ortiz@utelvt.edu.ec>
2025-06-03 20:17:07 -0500

    actualizacion

--- lib/main.dart
+++ lib/main.dart
@@ -468,7 +468,7 @@ class _EstadocivilPageState extends State<EstadocivilPage> {
     try {
       final response = await http.get(
         Uri.parse(
-          'https://educaysoft.org/whatsapp6a/app/controllers/EstadocivilController.php?action=api',
+          'https://educaysoft.org/microsoft6b/app/controllers/EstadocivilController.php?action=api',
         ),
       );
       if (response.statusCode == 200) {
